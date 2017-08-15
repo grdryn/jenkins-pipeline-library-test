8e1e241496f79889edd692a953286cfecb114db8
Gerard Ryan <gerard@ryan.lt>
2017-08-15 21:25:01 +0100

    🐛 Remove some other junk

--- src/com/company/utils/pipelineFunctions.groovy
+++ src/com/company/utils/pipelineFunctions.groovy
@@ -2,17 +2,12 @@ package com.company.utils;
 
 def run_in_stage(String stage_name, Closure command, String sendTo){
 
-    String ulink = gitTool.getCommitter()
-    String jlink = "(<${env.BUILD_URL}|Open>)"
-
-    println "============================================================"
     stage (stage_name) {
         try {
             command()
             if (currentBuild.result == 'FAILURE') {
                 error "Build failed, see log for further details."
             }
-            println "============================================================"
         } catch (Exception ex) {
             echo "Pipeline failed at stage: ${stage_name}"
             throw ex
